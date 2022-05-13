19898de66a940ace5677a0159a0f4575a864a85f
Андреев Антон <obsos32@gmail.com>
2022-05-13 12:42:30 +0300

    Проект спринта 8: CRUD для Yatube (v. 1.1)

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -85,20 +85,20 @@ DATABASES = {
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [{
-        'NAME': 'django.contrib.auth.password_validation.'
-                'UserAttributeSimilarityValidator',
+        'NAME': f'django.contrib.auth.password_validation.'
+                f'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'MinimumLengthValidator',
+        'NAME': f'django.contrib.auth.password_validation.'
+                f'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'CommonPasswordValidator',
+        'NAME': f'django.contrib.auth.password_validation.'
+                f'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'NumericPasswordValidator',
+        'NAME': f'django.contrib.auth.password_validation.'
+                f'NumericPasswordValidator',
     },
 ]
 
